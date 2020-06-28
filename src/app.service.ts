7f9854d380c83e183534e8bdaea58819a9b95c45
Renan Zulian <renanzulian@gmail.com>
2020-06-28 15:52:23 -0300

    getVersion would be nice to have

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getVersion(): Record<string, string> {
+    return { version: `App version ${process.env.APP_VERSION || '0.1.0'}` };
+  }
 }
